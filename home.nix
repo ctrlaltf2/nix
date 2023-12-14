dd77f206b81f2b914a8e863993a2c574c500aa4d
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2023-12-13 22:10:11 -0500

    Add nix-direnv

--- home.nix
+++ home.nix
@@ -18,7 +18,10 @@
   ];
 
   programs.bat.enable = true;
-  programs.direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
   programs.fzf.enable = true;
   programs.jq.enable = true;
   programs.lsd.enable = true;
