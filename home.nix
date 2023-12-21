3941498edc790de7ac4890d235fe84a6957ef0a1
ctrlaltf2 <github@troyer.dev>
2023-12-20 22:54:23 -0500

    Oops

--- home.nix
+++ home.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "caleb";
-  home.homeDirectory = "/home/caleb";
+  home.username = "user";
+  home.homeDirectory = "/home/user";
 
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
