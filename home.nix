1d88a67b24e454e5fc0726b6d4197e52fbfb20a8
ctrlaltf2 <github@troyer.dev>
2023-12-20 23:16:35 -0500

    Move dotfiles again, dotfiles managed by home-manager now

--- home.nix
+++ home.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "user";
-  home.homeDirectory = "/home/user";
+  home.username = "caleb";
+  home.homeDirectory = "/home/caleb";
 
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
@@ -150,12 +150,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
+    ".config/alacritty/alacritty.yml".source = .config/alacritty/alacritty.yml;
+    ".config/fontconfig/fonts.conf".source = .config/fontconfig/fonts.conf;
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
